150e59dc9eed429490cde07bd484091f728e34a3
Jasper Hwong Yu Chen <jasperhwong0077@gmail.com>
2022-02-08 11:46:56 +0800

    refactor: adding assoc to friends and user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
-  protected
-    def authenticate_user!
-      if user_signed_in?
-        super
-      else
-        redirect_to new_user_session_path, :notice => 'Please login.'
-        ## if you want render 404 page
-        ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-      end
-    end
+  # before_action :store_user_location!, if: :storable_location?
+  # before_action :authenticate_user!
+  # protected
+  #   def authenticate_user!
+  #     if user_signed_in?
+  #       super
+  #     else
+  #       redirect_to new_user_session_path, :notice => 'Please login.'
+  #       ## if you want render 404 page
+  #       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+  #     end
+  #   end
+  # private
+  #   def storable_location?
+  #     request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  #   end
+
+  #   def store_user_location!
+  #     # :user is the scope we are authenticating
+  #     store_location_for(:user, request.fullpath)
+  #   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_032354) do
+ActiveRecord::Schema.define(version: 2022_02_08_034223) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2022_02_07_032354) do
     t.string "instagram"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
